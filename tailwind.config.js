48d7f4df90b96c3271ac957b52179a6d3f8e0701
Amin Saroukhani <saroukani@gmail.com>
2025-02-12 13:37:12 +0330

    home page

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+    theme: {
+    extend: {
+      spacing:{
+        '5.5': '350px',
+      },
+      backgroundImage:{
+        'custom-image': "url('./images/header/banner.png.webp')",
+        'custom-image-dog':"url('./images/dog-service/service_icon_bg_1.png.webp')",
+        'custom-image-contact':"url('./images/header/contact_bg.png.webp')"
+      },
+      colors:{
+        prm:{
+          500: "#fc6518",
+          400:"#e82619",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
